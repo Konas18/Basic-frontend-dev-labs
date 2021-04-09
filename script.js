f79ad5c0a26610728dee33b7adbb3c235ab53e7c
Konas18 <konas2508@gmail.com>
2021-04-09 16:40:47 +0300

    Lab5

---
+++ script.js
@@ -0,0 +1,12 @@
+function Button() {
+  Swal.fire({
+    allowEscapeKey: "true",
+    background: "",
+    title: "",
+    iconColor: "green",
+    icon: "info",
+    title: " Вариант № 6",
+    text: "Макет был реализован Заболотским Александром Николаевичем.",
+    timer: "7000",
+  });
+}
